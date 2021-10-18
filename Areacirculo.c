51a6a3ad6b8f70964a17f974a7f7f15f1e4d5cfb
lesly-chaparro <89046801+lesly-chaparro@users.noreply.github.com>
2021-10-18 16:38:18 -0500

    Área del círculo

---
+++ Areacirculo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+#define PI 3.1416
+
+/*Área de un círculo 
+Lesly Chaparro*/
+
+int main(){
+    float area, radio; //Declaracion de variables
+    radio = 5; //Asignacion de valores, radio vale 5
+    area = PI * pow(radio,2); //Operacion, radio^2
+    printf("%s%f\n\n", "Área del círculo con radio 5:", area);
+}
