733cc10b1fb7050982827133364c01f6b0a3d150
lesly-chaparro <89046801+lesly-chaparro@users.noreply.github.com>
2021-11-04 16:22:57 -0600

    .

--- Variables.c
+++ Variables.c
@@ -4,6 +4,6 @@
 main (){
     printf("Lenguaje de programacion %c\n",'C');
     printf("Tengo %d anios\n",19);
-    printf("Dia % del 2021",14.11);
+    printf("Dia %f del 2021",14.11);
     return 0;
 }
