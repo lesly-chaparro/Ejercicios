870268eb6673f7437dee698d8491c1900e4f8cc9
lesly-chaparro <89046801+lesly-chaparro@users.noreply.github.com>
2021-11-04 22:05:27 -0600

    04-nov-2021

---
+++ Suma.c
@@ -0,0 +1,16 @@
+/*suma de dos numeros reales
+Lesly Chaparro */
+
+#include <stdio.h>
+int main(void){
+    //Declaracion de variables:
+    int num1, num2, resultado;
+    //Asignacion de valores:
+    num1 = 10;
+    num2 = 40;
+    //Operacion:
+    resultado = num1 + num2;
+    //Impresion del resultado:
+    printf("El resultado es: %d\n",resultado);
+    return 0;
+}
