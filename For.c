1ba82ab0de5121290c5a9cd47681a02e19329213
lesly-chaparro <89046801+lesly-chaparro@users.noreply.github.com>
2021-11-25 21:13:00 -0600

    .

---
+++ For.c
@@ -0,0 +1,16 @@
+/* ESTRUCTURA FOR
+clase 07-octubre
+Lesly Chaparro*/
+
+#include <stdio.h>
+
+int main(){
+    int cont;
+    //for (inicio; final; incremento/decremento)
+    for (cont = 1; cont <= 3; cont++){
+        printf("\tValor: %d\n",cont);
+    }
+    printf("\tValor final: %d\n",cont);
+return 0;
+}
+
